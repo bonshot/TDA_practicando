ce9b74df9cc3c13a73c69d89799be00903a76e96
root <root@DESKTOP-EJ9K31C>
2023-12-14 12:33:00 -0300

    finales terminados

--- Parciales/final_04.py
+++ Parciales/final_04.py
@@ -7,6 +7,37 @@
 # coincidiendo en la misma parada). Considerando que se tiene la información de todas las paradas de colectivo y qué
 # líneas paran allí, modelar el problema utilizando grafos e implementar un algoritmo que determine si es posible resolver
 # el problema. Indicar la complejidad del algoritmo implementado.
+def hay_conflictos(grafo, coloreados, v):
+    for w in grafo.adyacentes(v):
+        if coloreados[w] == coloreados[v]:
+            return True
+    return False
+
+def k_colectivos(grafo, k, vertices, coloreados):
+    if len(vertices) == 0:
+        return True
+    v = vertices.pop(0)
+    for color in range(k):
+        coloreados[v] = color
+        if not hay_conflictos(grafo, coloreados, v):
+            if k_colectivos(grafo, k, vertices, coloreados):
+                return True
+        coloreados[v] = None
+    vertices.insert(0, v)
+    return False
+
+def colectivos(grafo, k):
+    vertices = grafo.obtener_vertices()
+    coloreados = {}
+    return k_colectivos(grafo, k, vertices, coloreados)
+
+#La funcion hay_conflicos tiene complejidad O(grado(v)), donde grdo(v) es el grado del vertice en el grafo,
+#ya que recorre los vecinos del vertice v
+#la complejidad de k-colectivos, que utiliza un enfoque de backtracking y realiza un bucle sobre los colores posibles
+# k. En cada iteracion, se realiza la llamada recursiva a la funcion. En el peor de los casos
+# la complejidad es exponencial, siendo (K**n), donde n es el numero de vertices en el grafo
+
+#***************************************************************************************
 
 # 3. Dado un número n, mostrar la cantidad más económica (con menos términos) de escribirlo como una suma de cuadrados,
 # utilizando programación dinámica. Indicar y justificar el orden del algoritmo implementado.
@@ -17,9 +48,49 @@
 # es una manera más económica de escribirlo para n = 10, pues sólo tiene dos
 # términos. Además, tener en cuenta que no se piden los términos, sino la cantidad mínima de términos cuadráticos
 # necesaria.
-
+# def cuadrados(n):
+#     cant = ["inf"]*(n+1)
+#     cant[0] = 0
+#     sist = [x**2 for x in range(1,n)]
+#     for i in range(1, n+1):
+#         minimo = i
+#         for cuadrado in sist:
+#             if cuadrado > i:
+#                 continue
+#             cantidad = 1 + cant[i-cuadrado]
+#             if cantidad < minimo:
+#                 minimo = cantidad
+#         cant[i] = minimo
+#     return cant[n]
+# print(cuadrados(10))
 # 4. Realizar una reducción polinomial del problema del ejercicio 3 a otro de los vistos durante la cursada. Ayuda: pensar en
 # alguno de los vistos de programación dinámica.
+# este problema se asemeja al problema del cambio que ya sabemos que es Np-completo, 
+# Nosotros queremos ahora reducir este problema al problema del cambio y para eso tenemos que crear una instancia equivalente al problema del cambio
+#Definición del Problema de Cuadrados:
+
+# Dado un número entero n, el problema es encontrar la cantidad mínima de cuadrados perfectos (números de la forma x**2) necesarios para representar 
+# n como la suma de cuadrados perfectos.
+# Definición del Problema del Cambio (Monedas):
+# Dado un conjunto de denominaciones de monedas (cuadrados perfectos) y una cantidad objetivo 
+# n, el problema del cambio consiste en encontrar la cantidad mínima de monedas necesarias para alcanzar n.
+# Construcción de la Instancia del Problema de Cambio:
+# Utilizamos el conjunto de cuadrados perfectos menor o igual a n como denominaciones de monedas.
+# Mapeo de Términos Cuadráticos a Monedas:
+# Cada cuadrado perfecto en la instancia original se considera como una denominación de moneda en el problema de cambio.
+# Relación entre Soluciones:
+# La solución al problema de cambio (encontrar la cantidad mínima de monedas para alcanzar 
+# n) es directamente equivalente a la solución al problema original de suma de cuadrados (encontrar la cantidad mínima de cuadrados perfectos para representar n).
+#     Si podemos resolver el Problema de Cambio, podemos resolver el Problema de Cuadrados:
+#         Dada una solución eficiente al problema de cambio, podemos determinar la cantidad mínima de cuadrados perfectos necesarios para representar n.
+#         Cada moneda en el problema de cambio representa un cuadrado perfecto y la cantidad de monedas necesarias es la cantidad mínima de cuadrados perfectos para representar n.
+#     Si podemos resolver el Problema de Cuadrados, podemos resolver el Problema de Cambio:
+#         Dada una solución eficiente al problema de cuadrados, podemos utilizar esos cuadrados perfectos como denominaciones de monedas.
+#         Resolver el problema de cambio con estas denominaciones nos dará la cantidad mínima de monedas (cuadrados perfectos) necesarias para alcanzar n.
+# La complejidad de la resolución del problema de cambio determina la eficiencia de la resolución del problema original de suma de cuadrados después de la reducción.
+# La reducción no introduce una complejidad adicional; simplemente utiliza la estructura del problema de cambio para resolver el problema original de suma de cuadrados 
+# de manera eficiente.
+
 
 # 5. Dado un flujo máximo de un grafo, implementar un algoritmo que, si se le aumenta la capacidad a una artista, permita
 # obtener el nuevo flujo máximo en tiempo lineal en vértices y aristas. Indicar y justificar la complejidad del algoritmo
--- Parciales/final_11.py
+++ Parciales/final_11.py
@@ -1,5 +1,55 @@
 # 1. Implementar un algoritmo que, por backtracking, obtenga todos los posibles ordenamientos topológicos de un grafo
 # dirigido y acíclico.
+grafo = [
+    (1,2),
+    (2,4),
+    (3,4),
+    (4,5),
+    (1,5),
+    (3,6),
+    (4,6),
+]
+
+def nodos_grado_entrada_0(grafo, solucion_parcial):
+    aristas_restantes = set()
+    parcial = set(solucion_parcial)
+    nodos = set()
+    for arista in grafo:
+        s, d = arista
+        if s not in parcial:
+            nodos.add(s)
+        if d not in parcial:
+            nodos.add(d)
+
+        if s not in parcial and d not in parcial:
+            aristas_restantes.add(arista)
+
+    nodos_con_dependencias = set()
+
+    for arista in aristas_restantes:
+        s, d = arista
+        nodos_con_dependencias.add(d)
+
+    for n in nodos_con_dependencias:
+        if n in nodos:
+            nodos.remove(n)
+
+    return list(nodos)
+
+
+def _orden_topologico(grafo, solucion_parcial, soluciones):
+    nodos = nodos_grado_entrada_0(grafo, solucion_parcial)
+
+    if len(nodos) == 0:
+        soluciones.append(list(solucion_parcial))
+
+    for nodo in nodos:
+        solucion_parcial.append(nodo)
+        _orden_topologico(grafo, solucion_parcial, soluciones)
+        solucion_parcial.remove(nodo)
+
+soluciones = []
+_orden_topologico(grafo, [], soluciones)
 
 # 3. Dada una soga de n metros (n ≥ 2) implementar un algoritmo que, utilizando programación dinámica, permita cortarla
 # (en partes de largo entero) de manera tal que el producto del largo de cada una de las partes resultantes sea máximo. El
@@ -10,8 +60,33 @@
 # n = 5 --> Debe devolver 6 (producto máximo es 2 * 3)
 # n = 10 -> Debe devolver 36 (producto máximo es 3 * 3 * 4)
 
+cortes = dict()
+
+
+def calcular_cortes(n):
+    if cortes.get(n) != None:
+        return cortes[n]
+
+    m = 0
+    for i in range(1, n):
+        valor = i * calcular_cortes(n - i)
+        if valor > m:
+            m = valor
+    
+    if n > m:
+        m = n
+    
+    cortes[n] = m
+
+    return m
+
+print(calcular_cortes(10))
+print(calcular_cortes(11))
+
 # 4. Se tiene una matriz donde en cada celda hay submarinos, o no, y se quiere poner faros para iluminarlos a todos.
 # Implementar un algoritmo que Greedy que dé la cantidad mínima de faros que se necesitan para que todos los
 # submarinos queden iluminados, siendo que cada faro ilumina su celda y además todas las adyacentes (incluyendo las
 # diagonales), y las directamente adyacentes a estas (es decir, un “radio de 2 celdas”). Indicar y justificar la complejidad
 # del algoritmo implementado. ¿El algoritmo implementado da siempre la solución óptima?
+#El algoritmo no es optimo porque pueden haber mejores ubicaciones para ubicar los faros y asi
+#poner menos faros que la solucion dada por el algoritmo
